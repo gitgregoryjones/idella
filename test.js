f6bf39e352c2368e8a5b946ec5f06a0127e5a8bd
Greg Jones <gregory.jones@turner.com>
2019-07-17 16:40:56 -0400

    Add doc to js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const regex = new RegExp("(\\w+)\//+((?:\\w+)(?:-?\\w+)+).html","g");
 const str = `//www.cartoonnetwork.com//games//ren-stimpy.html`;
 let m;
+//A cool comment for NodeJS file
 
 while ((m = regex.exec(str)) !== null) {
     //// This is necessary to avoid infinite loops with zero-width matches
